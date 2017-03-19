38552618e3a6358d5b35e3c28dac630820a61406
numumotik <numumotik@mail.ru>
2017-03-20 00:25:31 +0300

    i don't know what i am doing now

--- app/src/main/java/com/discteam/e_wardrobe/WardrobeActivity.java
+++ app/src/main/java/com/discteam/e_wardrobe/WardrobeActivity.java
@@ -1,31 +1,18 @@
 package com.discteam.e_wardrobe;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
 
-public class WardrobeActivity extends FragmentActivity {
+public class WardrobeActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wardrobe);
-        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    protected Fragment createFragment(){
+        return WardrobeFragment.newInstance();
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
+    public static Intent newIntent(Context context) {
+        return new Intent(context, WardrobeActivity.class);
     }
 }
+
---
+++ app/src/main/java/com/discteam/e_wardrobe/WardrobeFragment.java
@@ -0,0 +1,127 @@
+package com.discteam.e_wardrobe;
+
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by Влада on 19.03.2017.
+ */
+
+public class WardrobeFragment extends Fragment {
+    private static final String GET_NUMBER = "getNumber";
+    private static final String PASS_NUMBER = "passNumber";
+
+    private Integer mCurrNumber;
+    private Button mNumberRequest;
+    private TextView mNumberView;
+    private boolean mCanGetNumber;
+
+    public static WardrobeFragment newInstance(){
+        return new WardrobeFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_wardrobe, container, false);
+        mNumberRequest = (Button)view.findViewById(R.id.numb_button);
+        mNumberView = (TextView)view.findViewById(R.id.number_text_view);
+        mCurrNumber = NumberPreferences.getNumber(getContext());
+        mCanGetNumber = mCurrNumber == 0;
+        updateNumber();
+
+        mNumberRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doNumberRequest(mCanGetNumber);
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_ecloset_menu, menu);
+        MenuItem notificationItem = menu.findItem(R.id.menu_item_notification);
+        boolean isAlarmOn = NumberPreferences.isAlarmOn(getContext());
+        if (isAlarmOn) {
+            notificationItem.setTitle(R.string.disable_notification);
+            notificationItem.setIcon(R.drawable.ic_menu_disable_notification);
+        } else {
+            notificationItem.setTitle(R.string.enable_notification);
+            notificationItem.setIcon(R.drawable.ic_menu_enable_notification);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_notification:
+                boolean isAlarmOn = NumberPreferences.isAlarmOn(getContext());
+                NotificationService.setServiceAlarm(getContext(), !isAlarmOn);
+                getActivity().invalidateOptionsMenu();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private class NumberRequestTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String opType = params[0];
+            switch(opType) {
+                case GET_NUMBER:
+                    return new NumberWorker(null).getNumber();
+
+                case PASS_NUMBER:
+                    new NumberWorker(mCurrNumber).passNumber();
+                    return null;
+
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String number) {
+            mCurrNumber = number == null ? 0 : Integer.parseInt(number);
+            NumberPreferences.setNumber(getContext(), mCurrNumber);
+            mCanGetNumber = !mCanGetNumber;
+            updateNumber();
+        }
+
+    }
+
+    public void doNumberRequest(boolean canGetNumber){
+        String opType = canGetNumber ? GET_NUMBER : PASS_NUMBER;
+        new NumberRequestTask().execute(opType);
+    }
+
+    public void updateNumber(){
+        mNumberView.setText(String.valueOf(mCurrNumber));
+        String requestInvitation = mCanGetNumber ? getActivity().getResources().getString(R.string.get_text)
+                : getActivity().getResources().getString(R.string.return_text);
+        mNumberRequest.setText(requestInvitation);
+    }
+}
+
